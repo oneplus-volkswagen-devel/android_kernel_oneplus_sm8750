db6363db6a9e1fe4c8e323ff4a41ca6a872a1927
SphericalKat <amolele@gmail.com>
2026-09-22 15:01:25 +0530

    soc: Import Nord 6 platform diagnostics
    Forward-port the Nord 6 Qualcomm SoC support delta.
    
    OSS repo: OnePlusOSS/android_kernel_oneplus_sm8735.
    
    Branch: oneplus/sm8735_b_16.0.0_nord_6.
    
    Source: kernel_platform/msm-kernel/drivers/soc/.
    
    AU tag: AU_LINUX_KERNEL.PLATFORM.4.0.R1.00.00.00.061.111.
    
    CLO old tag: KERNEL.PLATFORM.4.0.r1-11100-kernel.0.
    
    CLO new tag: KERNEL.PLATFORM.4.0.r1-16900-kernel.0.
    
    T_old: edadc33dca0bb889a2d7aa4588c782c89b71e5a3.
    
    T_new: 991664dbd9558868fc8300d86b99d0460dd73b8c.
    
    Inventory: kernel-rebase/records/nord6-kernel-stage1.md.
    Signed-off-by: SphericalKat <amolele@gmail.com>

--- drivers/soc/qcom/glink_probe.c
+++ drivers/soc/qcom/glink_probe.c
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: GPL-2.0-only
-/* Copyright (c) 2024 Qualcomm Innovation Center, Inc. All rights reserved. */
+/* Copyright (c) 2022 Qualcomm Innovation Center, Inc. All rights reserved. */
 
 #include <linux/of.h>
 #include <linux/list.h>
